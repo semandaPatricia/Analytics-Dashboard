de632362893bdc64bda71b587e88ab0988629e28
semanda <patash248@gmail.com>
2024-02-16 17:56:37 +0100

    Dummy data and redis setup

---
+++ src/employee.js
@@ -0,0 +1,22 @@
+const employeeData = [
+  { name: "John Marcus", Role: "Software Engineer", department: "Engineering" },
+  {
+    name: "Jane Smith",
+    Role: "Marketing Coordinator",
+    department: "Marketing",
+  },
+    { name: "Michael Johnson", Role: "Financial Analyst", department: "Finance" },
+    { name: "Emily Brown", Role: "HR Manager", department: "HR" },
+    { name: "David Williams", Role: "Network Administrator", department: "IT" },
+    { name: "Jessica Garcia", Role: "Sales Representative", department: "Sales" },
+    {
+    name: "Daniel Martinez",
+    Role: "Customer Service Specialist",
+    department: "Customer Service",
+  },
+  { name: "Olivia Wilson", Role: "Graphic Designer", department: "Creative" },
+  { name: "Ethan Taylor", Role: "Research Scientist", department: "R&D" },
+  { name: "Sophia Anderson", Role: "Legal Counsel", department: "Legal" },
+];
+
+export default employeeData;
